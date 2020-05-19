c0015e5838380e73096aaa2b624e09f6c31135fc
rhinehardtc <rhinehardtc@gmail.com>
2020-05-19 14:27:19 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+  # private
+
+  # def post_params
+  #   params = 
+  # end
   
 end
